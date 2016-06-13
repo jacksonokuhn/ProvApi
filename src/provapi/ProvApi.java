548086d11f5a2f870fbeef4bc446b9532fd255ec
Jackson Okuhn <jpokuhn@gmail.com>
2016-06-13 15:21:35 -0400

    node creation complete

--- src/provapi/ProvApi.java
+++ src/provapi/ProvApi.java
@@ -9,13 +9,100 @@ package provapi;
  *
  * @author jacksonokuhn
  */
+import java.util.Map;
+import CPL;
+        
 public class ProvApi {
 
+    private static Map<String, String> NAMESPACES;
+    
+    protected String[] args;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        CPL.attachODBC("DSN=CPL");
     }
     
+    public addNamespace(String key, String value){
+        
+            NAMESPACES.put(key, value);
+    }
+
+    public removeNamespace(String key){
+        
+            NAMESPACES.remove(key);
+    }
+    
+    public entity(){
+        
+    }
+    
+    public activity(){
+    
+    }
+    
+    public agent(){
+    
+    }
+    
+    public attributeValuePair(){
+        
+    }
+    
+    public wasGeneratedBy(){
+    
+    }
+    
+    public used(){
+    
+    }
+    
+    public wasInformedBy(){
+    
+    }
+    
+    public wasStartedBy(){
+    
+    }
+            
+    public wasEndedBy(){
+        
+    }
+    
+    public wasInvalidatedBy(){
+        
+    }
+    
+    public wasDerivedFrom(){
+    }
+    
+    public wasAttributedTo(){
+        
+    }
+    
+    public wasAssociatedWith(){
+        
+    }
+    
+    public actedOnBehalfOf(){
+        
+    }
+    
+    public wasInfluencedBy(){
+        
+    }
+    
+    public alternateOf(){
+        
+    }
+    
+    public specializationOf(){
+        
+    }
+    
+    public hadMember(){
+        
+    }   
 }
